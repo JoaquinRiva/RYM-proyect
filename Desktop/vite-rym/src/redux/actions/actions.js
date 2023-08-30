1bd73c7476c75e4887ff310218b02fbc159f44c8
Joaquin-Riva <rivajoaquin012@gmail.com>
2023-08-30 15:06:20 -0300

    agregando-filtros

--- Desktop/vite-rym/src/redux/actions/actions.js
+++ Desktop/vite-rym/src/redux/actions/actions.js
@@ -1,4 +1,10 @@
-import {ADD_TO_FAVORITES, REMOVE_FAVORITE} from "./action-types";
+import {
+  ADD_TO_FAVORITES,
+  REMOVE_FAVORITE,
+  SORT,
+  FILTER,
+  RESET,
+} from "./action-types";
 
 export function addFavorite(character) {
   return {
@@ -11,5 +17,25 @@ export function removeFavorite(id) {
   return {
     type: REMOVE_FAVORITE,
     payload: id,
+  };
+}
+
+export function filterByGender(gender) {
+  return {
+    type: FILTER,
+    payload: gender,
+  };
+}
+
+export function sortById(order) {
+  return {
+    type: SORT,
+    payload: order,
+  };
+}
+
+export function reset() {
+  return {
+    type: RESET,
   };
 }
