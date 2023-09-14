f6026beaa1726a3865a024987506761a31a73511
Joaquin-Riva <rivajoaquin012@gmail.com>
2023-09-14 15:55:08 -0300

    async

--- Desktop/vite-rym/api/src/controllers/getCharById.js
+++ Desktop/vite-rym/api/src/controllers/getCharById.js
@@ -1,20 +1,29 @@
 const axios = require("axios");
 
-function getCharById(req, res) {
-  const {id} = req.params;
-  axios
-    .get(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((response) => {
-      const {id, status, name, species, origin, image, gender} = response.data;
-      name
-        ? res
-            .status(200)
-            .json({id, status, name, species, origin, image, gender})
-        : res.status(404).send("Not Found Coleguillas");
-    })
-    .catch((error) => {
-      res.status(500).json({message: error.message});
-    });
+async function getCharById(req, res) {
+  const URL = "https://rickandmortyapi.com/api/character/";
+
+  try {
+    const {id} = req.params;
+    const {data} = await axios.get(`${URL}${id}`);
+
+    const character = {
+      id: data.id,
+      status: data.status,
+      name: data.name,
+      species: data.species,
+      location: data.location?.name,
+      origin: data.origin?.name, // asi no hay undefined
+      image: data.image,
+      gender: data.gender,
+    };
+
+    character.name
+      ? res.status(200).json(character)
+      : res.status(404).json({message: "Not Found Coleguillas"});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 }
 
 module.exports = getCharById;
